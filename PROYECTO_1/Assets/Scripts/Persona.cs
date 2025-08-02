cf4d03d68fedc849aa2d0af25755b6b10a8a8263
ayanitaa <ayanasalazar7@gmail.com>
2025-08-02 11:35:43 -0500

    creación Proyecto
    Se está creando el proyecto de Unity.

---
+++ PROYECTO_1/Assets/Scripts/Persona.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Persona : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Estoy en el start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Estoy en el update");
+    }
+}
