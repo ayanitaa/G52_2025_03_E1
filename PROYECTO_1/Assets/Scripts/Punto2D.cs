a9cf8d7df0569e458773e0b0a371d33266242d31
ayanitaa <ayanasalazar7@gmail.com>
2025-08-05 14:33:39 -0500

    commit Punto2D

---
+++ PROYECTO_1/Assets/Scripts/Punto2D.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System;
+
+namespace packagePunto2D
+{
+    [Serializable]
+
+    public class Punto2D : MonoBehaviour
+    {
+        private double x;
+        private double y;
+
+        public Punto2D()
+        {
+        }
+
+        public Punto2D(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+    }
+}
